87f91d7318f32c94d4bdc2e8b32c337d6663e15f
Rajibul <rajibul.seikh@eclick.co.in>
2026-02-20 10:48:11 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,50 @@
+"use client";
+import HomeBanner from "@/components/home/HomeBanner/HomeBanner";
+import { useGlobalContext } from "@/context/global_context";
+import { useEffect, useState } from "react";
+import { Stack } from "react-bootstrap";
 
-
-import Counter from '@/components/common/Counter';
-import CounterSection from '@/components/common/CounterSection';
-import TestimonialSection from '@/components/common/Testimonial';
-import Donation from '@/components/donation/Donation';
-import Brand from '@/components/home/Brand/Brand';
-import CharitableMessage from '@/components/home/CharitableMessage/CharitableMessage';
-import ChildEmpower from '@/components/home/CharitableMessage/ChildEmpower';
-import HomeBanner from '@/components/home/HomeBanner/HomeBanner';
-import HomeDescription from '@/components/home/HomeDescription/HomeDescription';
-import HomeProject from '@/components/home/HomeProject/HomeProject';
-import SuccessStory from '@/components/home/SuccessStory/SuccessStroy';
-import UrgentNeeds from '@/components/home/UrgentNeeds/UrgentNeeds';
-import USP from '@/components/home/Usp/Usp';
-import Volunteer from '@/components/home/Volunteer/Volunteer';
-import Ourreach from '@/components/our-reach/Ourreach';
-import { getHomeData } from '@/lib/api';
-
-const HomePage = async() => {
-    const data = await getHomeData();
-
-    const customFields = JSON.parse(data.pages_custom_field);
-    const aboutSection = customFields.group_name['about-section'];
-    const successStorySection = customFields.group_name['success-stroy-section'];
-    const projectSection = customFields.group_name['project-section'];
-
-    return (
-        <>
-            <HomeBanner banner={data.banner} />
-            <HomeDescription aboutSection={aboutSection} />
-            <USP usp={data.usp} />
-            <SuccessStory
-                sectionData={successStorySection}
-                stories={data.success_story}
-            />
-
-            <HomeProject
-                sectionData={projectSection}
-                projects={data.projects}
-            />
-
-            <Volunteer
-                sectionData={customFields.group_name['volunteer-section']}
-            />
-            <ChildEmpower
-                sectionData={customFields.group_name['empower-section']}
-                messages={data.charitable_message}
-            />
-
-            <UrgentNeeds
-                sectionData={customFields.group_name['urgent-needs-section']}
-            />
-            <Ourreach
-                sectionData={customFields.group_name['our-reach-section']}
-                ourReachData={data.our_reach}
-            />
-
-            <Counter 
-                className='home_counter'
-                poster={true}
-            />
-            <Donation />
-            <TestimonialSection
-                testimonials={data.testimonial}
-                customFields={customFields}
-                sectionKey="testimonial-section"
-                className="event-testimonials"
-            />
-
-            <Brand brands={data.donor_brand} />
-
-        </>
-    );
+interface BannerItem {
+    banner_name?: string;
+    banner_title?: string;
+    banner_description?: string;
+    banner_link?: string;
+    banner_file_link?: string;
+    banner_video_upload_type?: string;
 }
 
-export default HomePage;
+interface PageData {
+    banner: BannerItem | null;
+}
+
+const HomePage = () => {
+    const [data, setData] = useState<PageData | null>(null);
+    const {setHasLoading} = useGlobalContext();
+    const fetchData = async() => {
+        try{
+            setHasLoading(true);
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/home`, {cache: "no-cache"});
+            const {response_data} = await response.json();
+            setData(response_data);
+
+        }catch(err: unknown){
+            console.log('Home Page API fetch is something wrong: ', (err as Error).message)
+        }finally{
+            setHasLoading(false);
+        }
+    }
+
+    console.log('data', data)
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <Stack className="home_page">
+            <HomeBanner banner={data?.banner} />
+        </Stack>
+    )
+}
+
+export default HomePage
--- components/home/HomeBanner/HomeBanner.tsx
+++ components/home/HomeBanner/HomeBanner.tsx
@@ -7,21 +7,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { normalizeYouTubeUrl } from '@/utlis/videoUrl';
+import { useGlobalContext } from '@/context/global_context';
 
-interface HomeBannerProps {
-    banner: {
-        banner_title: string;
-        banner_name: string;
-        banner_description: string;
-        banner_link: string;
-        banner_file_link: string;
-        banner_video_upload_type: 'file' | 'link';
-    };
+interface BannerItem {
+    banner_name?: string;
+    banner_title?: string;
+    banner_description?: string;
+    banner_link?: string;
+    banner_file_link?: string;
+    banner_video_upload_type?: string;
 }
-
-const mediaBaseURL = process.env.NEXT_PUBLIC_MEDIA_URL;
-
-const HomeBanner = ({ banner }: HomeBannerProps) => {
+const HomeBanner = ({ banner }: { banner?: BannerItem | null }) => {
+    const { mediaUrl, hasLoading } = useGlobalContext();
     const [showVideo, setShowVideo] = useState<boolean>(false);
     const [videoUrl, setVideoUrl] = useState<string>("");
 
@@ -37,35 +34,44 @@ const HomeBanner = ({ banner }: HomeBannerProps) => {
         }, 300);
     };
 
-    if (!banner) return null;
+    // if (!banner) return null;
 
     return (
         <>
             <div className={`${Styles.innerbanner_sec} ${Styles.home_banner_sec}`}>
-                <CustomImage
-                    src={`${mediaBaseURL}${banner.banner_link}`}
-                    alt={banner.banner_title || "Inner-Banner"}
-                    className={`pt-0 ${Styles.inerbnrimg}`}
-                    fallBack='/assets/images/home_banner.jpg'
-                />
-                <div className={Styles.innerbannertxtbx}>
-                    <Container>
-                        <div className={`${Styles.inrbnrhead} ${Styles.homebnrhead}`}>
-                            <em
-                                dangerouslySetInnerHTML={{ __html: banner?.banner_name ?? "" }}
-                                style={{fontStyle: "normal"}}
-                            />  {banner?.banner_file_link && (
-                                <button
-                                    type="button"
-                                    onClick={() => handleOpenVideo(banner.banner_file_link)}
-                                    className={Styles.playBtn}
-                                >
-                                    <FontAwesomeIcon icon={faPlay} />
-                                </button>
-                            )}
+                {!hasLoading ? (
+                    <>
+                        <CustomImage
+                            src={`${mediaUrl}${banner?.banner_link}`}
+                            alt={banner?.banner_title || "Inner-Banner"}
+                            className={`pt-0 ${Styles.inerbnrimg}`}
+                            fallBack='/assets/images/home_banner.jpg'
+                        />
+                        <div className={Styles.innerbannertxtbx}>
+                            <Container>
+                                <div className={`${Styles.inrbnrhead} ${Styles.homebnrhead}`}>
+                                    <em
+                                        dangerouslySetInnerHTML={{ __html: banner?.banner_name ?? "" }}
+                                        style={{ fontStyle: "normal" }}
+                                    />  {banner?.banner_file_link && (
+                                        <button
+                                            type="button"
+                                            onClick={() => handleOpenVideo(banner.banner_file_link ?? '')}
+                                            className={Styles.playBtn}
+                                        >
+                                            <FontAwesomeIcon icon={faPlay} />
+                                        </button>
+                                    )}
+                                </div>
+                            </Container>
                         </div>
-                    </Container>
-                </div>
+                    </>
+
+                ) : (
+                    <>
+                        <div className="skeleton skeletonFill"></div>
+                    </>
+                )}
             </div>
             <Modal className="customBackdrop" show={showVideo} onHide={handleCloseVideo} size="xl" centered backdrop={false}>
                 <Modal.Header closeButton>
@@ -75,12 +81,12 @@ const HomeBanner = ({ banner }: HomeBannerProps) => {
                     {banner?.banner_video_upload_type == "file" ? (
                         <video width="100%" height="480" controls>
                             <source
-                                src={`${mediaBaseURL}${banner.banner_file_link}`}
+                                src={`${mediaUrl}${banner.banner_file_link}`}
                                 type="video/mp4"
                             />
                         </video>
                     ) : (
-                        <div style={{ position: "relative", paddingTop: "56.25%" /* 16:9 aspect ratio */ }}>
+                        <div style={{ position: "relative", paddingTop: "56.25%" }}>
                             <iframe width="100%" height="100%" src={videoUrl} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
                                 style={{ position: "absolute", top: 0, left: 0 }}></iframe>
                         </div>
