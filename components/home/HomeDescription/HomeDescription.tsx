876c3adab3d51683cf913a610d02a4f5283062be
Rajibul <rajibul.seikh@eclick.co.in>
2026-03-24 16:42:12 +0530

    layout shifting

--- components/home/HomeDescription/HomeDescription.tsx
+++ components/home/HomeDescription/HomeDescription.tsx
@@ -27,69 +27,107 @@ export default function HomeDescription({ aboutSection }: AboutInfo) {
     const { hasLoading } = useGlobalContext();
     useWOW({ animateClass: "animate__animated" });
 
-    if (!aboutSection) return null;
-
     return (
         <section className={Styles.posterAd}>
             <Container fluid className='px-0'>
                 <Row className="g-0 align-items-stretch">
                     <Col lg={6} className={Styles.left}>
-                        {!hasLoading ? (
+                        {!hasLoading && aboutSection ? (
                             <>
                                 <div className={Styles.leftContent}>
-                                    <div className={`wow animate__lightSpeedInRight ${Styles.smallTitle}`}>{aboutSection.about_subtitle}</div>
+                                    <div className={`wow animate__lightSpeedInRight ${Styles.smallTitle}`}>{aboutSection?.about_subtitle}</div>
 
                                     <h1 className={`wow animate__fadeInUp ${Styles.mainTitle}`}
                                         dangerouslySetInnerHTML={{
-                                            __html: aboutSection.about_title ?? '',
+                                            __html: aboutSection?.about_title ?? '',
                                         }}
                                     />
-                                    {aboutSection.about_left_button_text && (
+                                    {aboutSection?.about_left_button_text && (
                                         <Link
-                                            href={aboutSection.about_left_button_url ?? ''}
+                                            href={aboutSection?.about_left_button_url ?? ''}
                                             className={`wow animate__backInUp ${Styles.donateBtn}`}
                                             data-wow-delay="1s"
                                         >
-                                            {aboutSection.about_left_button_text}
+                                            {aboutSection?.about_left_button_text}
                                         </Link>
                                     )}
                                 </div>
                                 <CustomImage
-                                    src={`${mediaBaseURL}/uploads/page_image/${aboutSection.about_left_image}`}
+                                    src={`${mediaBaseURL}/uploads/page_image/${aboutSection?.about_left_image}`}
                                     alt="QR Code"
                                     className={`wow animate__pulse ${Styles.qrBox}`}
                                     data-wow-delay="1.5s"
                                 />
                             </>
                         ) : (
-                            <>Loader</>
+                            <>
+                                <div className="py-4 w-75">
+                                    <div className={`skeleton skeletonHeightText w-25 ${Styles.smallTitle}`}>&nbsp;</div>
+                                    <br />
+                                    <p className="skeleton mb-2">&nbsp;</p>
+                                    <p className="skeleton mb-2">&nbsp;</p>
+                                    <p className="skeleton mb-2">&nbsp;</p>
+                                    <p className="skeleton mb-2 w-75">&nbsp;</p>
+                                    <p className="skeleton mb-2 w-50">&nbsp;</p>
+                                    <span
+                                        className={`skeleton skeletonBtn ${Styles.donateBtn}`}
+                                        data-wow-delay="1s"
+                                    >&nbsp;</span>
+                                </div>
+                                <div className={`custom_image fixedImage ${Styles.qrBox}`}>
+                                    <div className={`skeleton ${Styles.qrBoxSkeleton}`}></div>
+                                </div>
+                            </>
                         )}
 
                     </Col>
-                    
+
                     <Col lg={6} className={Styles.right}>
-                        <div className={Styles.rightContent}>
-                            <div
-                                dangerouslySetInnerHTML={{
-                                    __html: aboutSection.about_right_description ?? '',
-                                }}
-                                className='wow animate__slideInDown'
-                            />
-                            {aboutSection.about_right_button_text && (
-                                <Link
-                                    href={aboutSection.about_right_button_url ?? ''}
-                                    className={`wow animate__slideInUp ${Styles.secondaryBtn}`}
-                                >
-                                    {aboutSection.about_right_button_text}
-                                </Link>
-                            )}
-                        </div>
-                        {aboutSection.about_right_image && (
-                            <CustomImage
-                                src={`${mediaBaseURL}/uploads/page_image/${aboutSection.about_right_image}`}
-                                alt="About Image"
-                                className={`wow animate__slideInRight ${Styles.imageWrap}`}
-                            />
+                        {!hasLoading && aboutSection ? (
+                            <>
+                                <div className={Styles.rightContent}>
+                                    <div
+                                        dangerouslySetInnerHTML={{
+                                            __html: aboutSection?.about_right_description ?? '',
+                                        }}
+                                        className='wow animate__slideInDown'
+                                    />
+                                    {aboutSection?.about_right_button_text && (
+                                        <Link
+                                            href={aboutSection?.about_right_button_url ?? ''}
+                                            className={`wow animate__slideInUp ${Styles.secondaryBtn}`}
+                                        >
+                                            {aboutSection?.about_right_button_text}
+                                        </Link>
+                                    )}
+                                </div>
+                                {aboutSection?.about_right_image && (
+                                    <CustomImage
+                                        src={`${mediaBaseURL}/uploads/page_image/${aboutSection?.about_right_image}`}
+                                        alt="About Image"
+                                        className={`wow animate__slideInRight ${Styles.imageWrap}`}
+                                    />
+                                )}
+                            </>
+                        ) : (
+                            <>
+                               <div className={`w-75 ${Styles.rightContent}`}>
+                                    <div className={`skeleton skeletonHeightText w-25 ${Styles.smallTitle}`}>&nbsp;</div>
+                                    <br />
+                                    <div className="skeleton mb-2">&nbsp;</div>
+                                    <div className="skeleton mb-2">&nbsp;</div>
+                                    <div className="skeleton mb-2">&nbsp;</div>
+                                    <div className="skeleton mb-2 w-75">&nbsp;</div>
+                                    <div className="skeleton mb-2 w-50">&nbsp;</div>
+                                    <span
+                                        className={`skeleton skeletonBtn ${Styles.secondaryBtn}`}
+                                        data-wow-delay="1s"
+                                    >&nbsp;</span>
+                                </div>
+                                <div className={`custom_image fixedImage ${Styles.imageWrap}`}>
+                                    <div className="skeleton" style={{aspectRatio: 1/1}}></div>
+                                </div>
+                            </>
                         )}
                     </Col>
 
--- components/home/Usp/Usp.tsx
+++ components/home/Usp/Usp.tsx
@@ -1,6 +1,8 @@
+'use client';
 import Styles from "./style.module.css"
 import { Col, Container, Row } from 'react-bootstrap';
 import CustomImage from '@/utlis/imagefunction';
+import { useGlobalContext } from "@/context/global_context";
 
 interface USPItem {
     usp_title?: string;
@@ -15,32 +17,48 @@ interface USPProps {
 const mediaBaseURL = process.env.NEXT_PUBLIC_MEDIA_URL;
 
 const USP = ({ usp }: USPProps) => {
-    if (!usp || usp.length === 0) return null;
+    // if (!usp || usp.length === 0) return null;
+    const { hasLoading } = useGlobalContext();
 
     return (
         <div className={Styles.usp_section}>
             <Container>
                 <Row className={`g-4 ${Styles.row ?? ''}`}>
-                    {usp.map((item, index) => (
-                        <Col lg={3} sm={6} key={index} className={Styles.cardItem}>
-                            <div className={Styles.card}>
-                                {item.usp_feature_image && (
-                                    <CustomImage
-                                        src={`${mediaBaseURL}${item.usp_feature_image}`}
-                                        alt={item.usp_title}
-                                        className={Styles.cardImage}
+                    {!hasLoading ? (
+                        usp.map((item, index) => (
+                            <Col lg={3} sm={6} key={index} className={Styles.cardItem}>
+                                <div className={Styles.card}>
+                                    {item.usp_feature_image && (
+                                        <CustomImage
+                                            src={`${mediaBaseURL}${item.usp_feature_image}`}
+                                            alt={item.usp_title}
+                                            className={Styles.cardImage}
+                                        />
+                                    )}
+                                    <h3>{item.usp_title}</h3>
+                                    <div
+                                        className={Styles.card_content}
+                                        dangerouslySetInnerHTML={{
+                                            __html: item.usp_description ?? ''
+                                        }}
                                     />
-                                )}
-                                <h3>{item.usp_title}</h3>
-                                <div
-                                    className={Styles.card_content}
-                                    dangerouslySetInnerHTML={{
-                                        __html: item.usp_description ?? ''
-                                    }}
-                                />
-                            </div>
-                        </Col>
-                    ))}
+                                </div>
+                            </Col>
+                        ))) : (
+                        [...Array(4)].map((_, index) => (
+                            <Col lg={3} sm={6} key={index} className={Styles.cardItem}>
+                                <div className={Styles.card}>
+                                    <div className={`skeleton ${Styles.cardImage}`}></div>
+                                    <h3 className="skeleton w-75">&nbsp;</h3>
+                                    <div className={`w-100 ${Styles.card_content}`}>
+                                        <p className="skeleton w-100 skeletonHeightText mb-2">&nbsp;</p>
+                                        <p className="skeleton w-75 mx-auto skeletonHeightText mb-2">&nbsp;</p>
+                                        <p className="skeleton w-50 mx-auto skeletonHeightText mb-2">&nbsp;</p>
+                                    </div>
+                                </div>
+                            </Col>
+                        ))
+                    )}
                 </Row>
             </Container>
         </div>
